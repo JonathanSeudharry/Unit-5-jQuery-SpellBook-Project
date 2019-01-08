5572809257674890455f160d7e48688103b59a3a
JonathanSeudharry <43378163+JonathanSeudharry@users.noreply.github.com>
2019-01-08 09:47:19 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,26 @@
+//Spell #1
+
+$("  .spell1   ").click(function(){
+    $(".picone").hide();
+});
+
+
+
+//Spell #2
+
+$("  .spell2    ").click(function(){
+    $(".pictwo").slideUp();
+});
+
+
+
+//Spell #3
+
+$("  .spell3    ").click(function(){
+     $(".picthree").fadeOut();
+});
+
+
+
+
+
